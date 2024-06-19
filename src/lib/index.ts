f9087f3628ba69a3673fc68ceb46537b95d365f8
Sope Shen <shenshouer51@gmail.com>
2024-06-19 18:07:27 +0800

    添加sleep方法

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
+export async function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
