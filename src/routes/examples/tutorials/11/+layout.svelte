d3f89f9b5318f10c22f6a918753888ec5f41a04a
Sope Shen <shenshouer51@gmail.com>
2024-06-19 19:21:53 +0800

    事件修饰器

--- src/routes/examples/tutorials/11/+layout.svelte
+++ src/routes/examples/tutorials/11/+layout.svelte
@@ -2,6 +2,7 @@
     <h3>事件</h3>
     <nav>
         <a href="/examples/tutorials/11/dom-event">事件</a>
+        <a href="/examples/tutorials/11/event-modifiers">事件装饰器</a>
     </nav>
 </aside>
 
---
+++ src/routes/examples/tutorials/11/event-modifiers/+page.svelte
@@ -0,0 +1,6 @@
+<h3>事件装饰器</h3>
+<a href="https://learn.svelte.dev/tutorial/event-modifiers">参考</a>
+<br />
+
+<p>只触发一次</p>
+<button on:click|once={() => alert("clicked")}> Click me </button>
